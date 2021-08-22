0cb8500fd9262e7009c47398fd0f7389471cbc8b
megnt1 <whddbs9812@gmail.com>
2021-08-22 15:33:47 +0900

    ex01.cpp 추가

---
+++ chapter01/ex01.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // 
+using namespace std;
+    // main 함수 entry point(진입점)
+int main(int argc, char const *argv[]){
+    cout << "Hello World" << endl;
+    return 0;   // 파이썬: sys.exit(0)
+}
+// {} 범위지정 int<-- 리턴하는 함수, c는 console의 약자, endl= \n
+// using namespace std<-- 를 안썼다면 std::cout << "Hello World----"<<std::endl;  
